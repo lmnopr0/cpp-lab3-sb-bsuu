785bf0ad9a0e79f93d21f1f4374716f6eea76c3b
lmnopr0 <114658160+lmnopr0@users.noreply.github.com>
2022-12-22 08:55:56 +0300

    Update 3a-t4.cpp

--- 3a-t4.cpp
+++ 3a-t4.cpp
@@ -1,15 +1,26 @@
 #include<iostream> 
-using namespace std; 
+using namespace std;
 
 int main()
 {
-int k, i = 1, sum_k = 0, number;
-cout << " Enter the amount of integers to sum = "; cin >> k;
-do {
-cout << "Enter integer nr. " << i << ": "; 
-cin >> number;
-sum_k += number; i++;
-} while (i <= k);
-cout << " The total sum of " << k << " integers is: "  << sum_k;
-return 0;
+	int k, i = 1, sum_k = 0, number;
+	cout << " Enter the amount of integers to sum = "; cin >> k;
+	if (cin.fail()) {
+		cout << " Fail" << endl;
+		cin.clear();
+		cin.ignore();
+		cout << " Enter once more = ";
+		cin >> k;
+	}
+	if (k == 0) cout << " There is nothing to sum";
+	else {
+		do {
+			cout << "Enter integer nr. " << i << ": ";
+			cin >> number;
+			sum_k += number; i++;
+		} while (i <= k);
+		cout << " The total sum of " << k << " integers is: " << sum_k;
+	}
+	return 0;
 }
+
